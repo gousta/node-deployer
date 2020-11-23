f423b9e6d64bc271ab8ccecb532edb28ab74d28d
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2020-11-23 12:27:15 +0200

    Added body parser for reading body

--- node-deployer.js
+++ node-deployer.js
@@ -1,24 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const exec = require('child_process').exec;
-const credentials  = require('./credentials.json');
-const port = 3939
+const credentials = require('./credentials.json');
+const bodyParser = require('body-parser');
+const port = 3939;
 
 const regex = /[^a-zA-Z0-9.]/g; // cleaner regex
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.post('/___________/:repo', (req, res) => {
   const { repo } = req.params;
   const payload = req.body;
-  
+
   console.log('payload', JSON.stringify(payload, null, 2));
-  
-  if(credentials.secret === payload.hook.config.secret && repo) {
+
+  if (credentials.secret === payload.hook.config.secret && repo) {
     const cleaned = repo.replace(regex, repo);
 
     exec(`cd /var/www/${cleaned} && git pull`);
   }
-  
+
   res.send(`OK: ${repo}`);
 });
 
-app.listen(port, () => console.log(`NODE DEPLOYER RUNNING ON PORT ${port}!`))
+app.listen(port, () => console.log(`NODE DEPLOYER RUNNING ON PORT ${port}!`));
