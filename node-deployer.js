b7a53efdb1cb19acc3695ad3003cec140abdb71e
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2020-11-23 12:18:59 +0200

    Update node-deployer.js

--- node-deployer.js
+++ node-deployer.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const app = express()
 const exec = require('child_process').exec;
+const credentials  = require('credentials.json');
 const port = 3939
 
 const regex = /[^a-zA-Z0-9.]/g; // cleaner regex
 
 app.post('/___________/:repo', (req, res) => {
   const { repo } = req.params;
-  const cleaned = repo.replace(regex, repo);
+  const { secret } = req.body;
+  
+  if(credentials.secret === secret && repo) {
+    const cleaned = repo.replace(regex, repo);
 
-  exec(`cd /var/www/${cleaned} && git pull`);
-
+    exec(`cd /var/www/${cleaned} && git pull`);
+  }
+  
   res.send(`OK: ${repo}`);
 });
-app.listen(port, () => console.log(`NODE DEPLOYER ${port}!`))
+
+app.listen(port, () => console.log(`NODE DEPLOYER RUNNING ON PORT ${port}!`))
